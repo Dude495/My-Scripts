b4f8d3a23d0ed0df884304bad05f8d4f520b2462
Dude495 <40000573+Dude495@users.noreply.github.com>
2018-08-09 11:14:09 -0400

    Add files via upload

--- WME NY UR Project Timer.js
+++ WME NY UR Project Timer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME NY UR Project Timer
 // @namespace    Dude495
-// @version      2018.08.02.001
+// @version      2018.08.09.001
 // @description  Adds count down timer for the NY UR Project (2018).
 // @author       Dude495
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
@@ -13,33 +13,33 @@
 
 (function() {
     'use strict';
-function startClock() {
- var PHASE = 'Brooklyn'
-    var startTime = new Date('aug 11, 2018 00:00:00 EDT').getTime();
-    var now = new Date().getTime();
-    var time = startTime - now;
-    var days = Math.floor (time / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((time%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-    var minutes = Math.floor((time % (100 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((time % (1000 * 60)) / 1000);
-    var div = [];
-    if (time > 0) {
-        div = $('<div>', {id: 'countdown-timer'}).css({marginBottom:'3px', paddingLeft:'2px', textAlign:'center', fontWeight:'600', background: 'yellow'});
-    }
-    if (time < 0) {
-        div = $('<div>', {id: 'countdown-timer'}).css({marginBottom:'3px', paddingLeft:'2px', textAlign:'center', fontWeight:'600', background: 'lime'});
-    }
+    function startClock() {
+        var PHASE = 'Brooklyn'
+        var startTime = new Date('aug 11, 2018 00:00:00 EDT').getTime();
+        var now = new Date().getTime();
+        var time = startTime - now;
+        var days = Math.floor (time / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((time%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+        var minutes = Math.floor((time % (100 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((time % (1000 * 60)) / 1000);
+        var div = [];
+        if (time > 0) {
+            div = $('<div>', {id: 'countdown-timer'}).css({marginBottom:'3px', paddingLeft:'2px', textAlign:'center', fontWeight:'600', background: 'yellow'});
+        }
+        if (time < 0) {
+            div = $('<div>', {id: 'countdown-timer'}).css({marginBottom:'3px', paddingLeft:'2px', textAlign:'center', fontWeight:'600', background: 'lime'});
+        }
         if ($('#countdown-timer').length <= 0) {
             div;
             $('#user-box').after(div);
-        $('#user-profile').css('margin-bottom','5px');
+            $('#user-profile').css('margin-bottom','5px');
         }
         $('#user-box').css('padding-bottom','5px');
         document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' phase begins in ' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
         if (time < 0) {
-            document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' phase has ended!' + '<br>' + 'Thank you for your support.';
+            document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' phase has began, Happy Editing!';
         }
-}
+    }
 
     function bootstrap() {
         if (W && W.loginManager && W.loginManager.isLoggedIn()) {
