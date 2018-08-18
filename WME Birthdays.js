3c80ad1d896f4cc3bfd1af249f35abf8ce72a3ae
Dude495 <40000573+Dude495@users.noreply.github.com>
2018-08-17 21:03:07 -0400

    Update WME Birthdays.js

--- WME Birthdays.js
+++ WME Birthdays.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Birthdays
 // @namespace    Dude495
-// @version      2018.08.17.002
+// @version      2018.08.17.003
 // @description  Creates buttons on the top bar of the Waze Forums to access editor birthday information.
 // @author       Dude495
 // @include      /^https:\/\/.*\.waze\.com\/forum\/.*
@@ -109,10 +109,10 @@
             };
         };
         sigbox.onclick = function() {
-            if (sigbox.checked == true) {
+            if (sigbox.checked) {
                 localStorage.setItem('SIG', 'true');
             };
-            if (sigbox.checked == false) {
+            if (!sigbox.checked) {
                 localStorage.setItem('SIG', 'false');
             };
         };
