29ff46566e7ce1aaaf8c33cb0346ce20b6faede9
Dude495 <40000573+Dude495@users.noreply.github.com>
2019-03-16 18:57:50 -0400

    Update WME Land of the Pure Timer.js

--- WME Land of the Pure Timer.js
+++ WME Land of the Pure Timer.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Land of the Pure Timer
 // @namespace    Dude495
-// @version      2019.03.14.01
+// @version      2019.03.16.01
 // @description  Adds count down timer for the Land of the Pure (Pakistan) WoW
 // @author       Dude495
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
@@ -28,14 +28,14 @@
         if (ProjStatus == 'false') {
             if (time > 18000001) {
                 div = $('<div>', {id: 'countdown-timer'}).css({marginBottom:'3px', paddingLeft:'2px', textAlign:'center', fontWeight:'600', background: 'red'});
-            };
+            }
             if ((time < 18000000) && (time > 0)) {
                 div = $('<div>', {id: 'countdown-timer'}).css({marginBottom:'3px', paddingLeft:'2px', textAlign:'center', fontWeight:'600', background: 'yellow'});
-            };
+            }
             if (time < 0) {
                 div = $('<div>', {id: 'countdown-timer'}).css({marginBottom:'3px', paddingLeft:'2px', textAlign:'center', fontWeight:'600', background: 'lime'});
-            };
-        };
+            }
+        }
         if ($('#countdown-timer').length <= 0) {
             div;
             $('#user-box').after(div);
@@ -43,28 +43,34 @@
         }
         $('#user-box').css('padding-bottom','5px');
         if (ProjStatus == 'false') {
-            document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW begins in ' + weeks + 'w ' + days + 'd ' + hours + 'h ' + minutes + 'm ';
-            if((time < 604800000) && (time >= 18000001)) {
+            if (time > 604800000) {
+                document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW begins in ' + weeks + 'w ' + days + 'd ' + hours + 'h ' + minutes + 'm ';
+            }
+            else if ((time < 604800000) && (time >= 18000001)) {
                 document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW begins in ' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
-            };
-                if (time < 0) {
+            }
+            else if ((time <= 18000000) && (time >= 1)) {
+                document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW begins in ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+            }
+            else if (time < 0) {
                 document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW has started, Happy Editing!';
-            };
-        };
+            }
+        }
         if (ProjStatus == 'true') {
-            document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW ends in ' + weeks + 'w ' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
-            if((time < 604800000) && (time >= 18000001)) {
+            if (time > 604800000) {
+                document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW ends in ' + weeks + 'w ' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+            }
+            else if ((time < 604800000) && (time >= 18000001)) {
                 document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW ends in ' + days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
-            };
-            if ((time <= 18000000) && (time > 0)) {
+            }
+            else if ((time <= 18000000) && (time > 0)) {
                 document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW ends in ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
-            };
-            if (time < 0) {
+            }
+            else if (time < 0) {
                 document.getElementById('countdown-timer').innerHTML = 'The ' + PHASE + ' WoW has ended! <br> Thank you for all your efforts!';
-            };
-        };
-    };
-
+            }
+        }
+    }
     function bootstrap() {
         if (W && W.loginManager && W.loginManager.isLoggedIn()) {
             setInterval(startClock, 1000);
