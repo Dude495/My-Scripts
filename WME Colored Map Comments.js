0bb34a239919aa8e11c2f9e13e980f1ca0c32c4f
Dude495 <40000573+Dude495@users.noreply.github.com>
2019-03-30 18:08:30 -0400

    Update WME Colored Map Comments.js

--- WME Colored Map Comments.js
+++ WME Colored Map Comments.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         WME Colored Map Comments
 // @namespace    Dude495
-// @version      2019.03.26.02
+// @version      2019.03.30.01
 // @author       Dude495
-// @description  Change the color of Map Comments based on HEX Color Code.
+// @description  Change the color of Map Comment Points based on HEX Color Code.
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
 // @require      https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
 // @license      GNU GPLv3
@@ -31,8 +31,9 @@
     function Save() {
         localStorage.setItem('CMC', $('#CMC').val());
         ChangeColor();
+        $('#colorWheel')[0].value = localStorage.getItem('CMC');
     }
-    function init() { //Copied from WME HardHats by RickZ
+    function init() { //Side Panel based off code from RickZ via HardHats
         $('#sidepanel-prefs').append($("<div>").css("clear", "both")
                                      .css("margin-bottom", "10px")
                                      .append($("<h5>").html("Colored Map Comments v"+GM_info.script.version+",")
@@ -42,21 +43,35 @@
                                      .append($('<textarea rows="1" cols="10" maxlength="7">').attr("id", "CMC")
                                              .attr("title", "Enter the HEX Color Code for the color you want Map Comments to appear. (Include the #)")
                                              .css("resize", "none")
+                                             .css("width", "60px")
+                                             .css("height", "25px")
                                             )
-                                     .append($('<br><button class="btn btn-success">Save</button>')
+                                     .append($('<input id="colorWheel" type="color" name="favcolor" value="'+localStorage.getItem("CMC")+'">')
+                                             .css("margin-left", "5px")
+                                             .css("top", "-10px")
+                                             .css("position", "relative")
+                                             .css("width", "25px")
+                                            )
+                                     .append($('<br><button class="btn btn-success" data-toggle="tooltip">Save</button>')
                                              .click(Save)
-                                             .attr("title", "WME Colored Map Comments")
+                                             .attr("title", "This button is only needed if you manually change the HEX Code")
                                              .css("margin-bottom", "50px")
                                             )
                                     );
+        $('#colorWheel')[0].onchange = function() {
+            $('#CMC')[0].value = $('#colorWheel')[0].value;
+            Save();
+        }
         $("#CMC").val(localStorage.getItem('CMC'));
         ChangeColor();
+        $('[data-toggle="tooltip"]').tooltip();
     }
     function bootstrap() {
         if (W && W.loginManager && W.loginManager.user && WazeWrap.Ready) {
             console.log(GM_info.script.name, 'Initialized');
             init();
             WazeWrap.Events.register("moveend", null, ChangeColor);
+            WazeWrap.Interface.ShowScriptUpdate(GM_info.script.name, GM_info.script.version, "<ul><li>Added a color wheel picker</li></ul>", "https://greasyfork.org/en/scripts/380974-wme-colored-map-comments","");
         } else {
             console.log(GM_info.script.name, 'Bootstrap failed.  Trying again...');
             window.setTimeout(() => bootstrap(), 500);
